00f208b012def1269094fad3e03cdbb343d0233c
loket <loket@cruftlab.com>
2022-12-01 20:42:28 +0100

    fix: rename aoc to advent_of_code

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,5 +1,3 @@
-use std::ops::Add;
-
 fn calories_from_input(input: &str) -> Box<dyn Iterator<Item = u32>> {
     let mut calories = vec![0u32];
     // Temporarily store lines as Some(u32)
@@ -33,9 +31,9 @@ pub fn part_two(input: &str) -> Option<u32> {
 }
 
 fn main() {
-    let input = &aoc::read_file("inputs", 1);
-    aoc::solve!(1, part_one, input);
-    aoc::solve!(2, part_two, input);
+    let input = &advent_of_code::read_file("inputs", 1);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
 }
 
 #[cfg(test)]
@@ -44,13 +42,13 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let input = aoc::read_file("examples", 1);
+        let input = advent_of_code::read_file("examples", 1);
         assert_eq!(part_one(&input), Some(24000));
     }
 
     #[test]
     fn test_part_two() {
-        let input = aoc::read_file("examples", 1);
+        let input = advent_of_code::read_file("examples", 1);
         assert_eq!(part_two(&input), Some(45000));
     }
 }

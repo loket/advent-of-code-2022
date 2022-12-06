f93046db91c3a15cd16b5ba72e9f35ac7316ed90
loket <loket@cruftlab.com>
2022-12-06 21:15:00 +0100

    solve day 6 part 2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -2,28 +2,59 @@ use std::collections::HashSet;
 
 use itertools::Itertools;
 
+type StartOfPacket = (char, char, char, char);
+type StartOfMessage = (char, char, char, char, char, char, char, char, char);
+
 // TODO: Don't hard-code the window size
-pub fn first_marker(input:&str) -> u32 {
+/*
+pub fn first_marker(input: &str) -> u32 {
     // Create sliding window of 4 characters
-    let windows = input.chars().tuple_windows::<(_,_,_,_)>();
+    let windows = input.chars().tuple_windows::<StartOfPacket>();
     // Enumerate all characters
     let e = windows.enumerate();
     // Skip until there's a match in first-of-packet marker
     let mut skipped = e.skip_while(|(i, chars)| {
-        let set: HashSet<char> = vec![chars.0, chars.1, chars.2, chars.3].into_iter().collect();
+        let set: HashSet<char> = vec![chars.0, chars.1, chars.2, chars.3]
+            .into_iter()
+            .collect();
         set.iter().count() != 4
     });
     // Return the index, plus offset 4
     skipped.next().unwrap().0 as u32 + 4
 }
+*/
+
+fn first_marker(input: &str, window_size: usize) -> u32 {
+    let range_end = input.len() - window_size;
+    let mut range = 0..range_end - 1;
+    let chars: Vec<char> = input.chars().collect();
+    let matched = range
+    .find(|i| {
+        let window = chars.iter().skip(*i).take(window_size);
+        let window_set: HashSet<&char> = window.into_iter().collect();
+        window_set.iter().count() == window_size
+    })
+    .unwrap();
+    let result = matched + window_size;
+    result as u32
+}
+
+fn first_start_of_packet_marker(input: &str) -> u32 {
+    first_marker(&input, 4)
+}
+
+fn first_start_of_message_marker(input: &str) -> u32 {
+    first_marker(&input, 14)
+}
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let marker = first_marker(input);
+    let marker = first_start_of_packet_marker(input);
     Some(marker)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let marker = first_start_of_message_marker(input);
+    Some(marker)
 }
 
 fn main() {
@@ -39,28 +70,28 @@ mod tests {
     #[test]
     fn test_example_1() {
         let input = "bvwbjplbgvbhsrlpgdmjqwftvncz";
-        let marker = first_marker(input);
+        let marker = first_start_of_packet_marker(input);
         assert_eq!(marker, 5);
     }
 
     #[test]
     fn test_example_2() {
         let input = "nppdvjthqldpwncqszvftbrmjlhg";
-        let marker = first_marker(input);
+        let marker = first_start_of_packet_marker(input);
         assert_eq!(marker, 6);
     }
 
     #[test]
     fn test_example_3() {
         let input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
-        let marker = first_marker(input);
+        let marker = first_start_of_packet_marker(input);
         assert_eq!(marker, 10);
     }
 
     #[test]
     fn test_example_4() {
         let input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
-        let marker = first_marker(input);
+        let marker = first_start_of_packet_marker(input);
         assert_eq!(marker, 11);
     }
 
